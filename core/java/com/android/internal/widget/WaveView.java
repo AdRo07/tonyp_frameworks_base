774b20afe694f5b241755801acd595381bb8b55e
DerTeufel <dominik-kassel@gmx.de>
2012-11-06 18:21:05 +0100

    blackberry lock: easier unlocking?
    Change-Id: I4b3b696900160839fd5cce481afc5d5ff45fba55

--- core/java/com/android/internal/widget/WaveView.java
+++ core/java/com/android/internal/widget/WaveView.java
@@ -196,9 +196,9 @@ public class WaveView extends View implements ValueAnimator.AnimatorUpdateListen
 
 	int screenHeight = getHeight();
             if (screenHeight < 1280) {
-                mRingRadius = 800.0f;
+                mRingRadius = 790.0f;
             } else {
-                mRingRadius = 1280.0f;
+                mRingRadius = 1270.0f;
             }
 
 
