fd9585e02f70cfe1181ac71071258b4589b7271c
Lars Greiss <kufikugel@googlemail.com>
2013-04-25 18:18:37 +0800

    Frameworks: Advanced low battery indicator options (1/2) Based on a idea I saw on Liquid and from CR3C0 (thanks guys) which we modified to an extended version
    This commit introduces following new SlimBean modes to indicate low battery
    
         * 0 = show dialog + play sound (default)
         * 1 = fire notification + play sound
         * 2 = show dialog only
         * 3 = fire notification only
         * 4 = play sound only
         * 5 = none
    
    Due that notification is a unique SlimBean feature some words on it. Notification
    behaviour is the same as Pop-up. User can dismiss it manually or it is
    dismissed automatically if charger is connected, click on notification opens
    PowerUsageSummary. Also you get a status bar ticker message about the notification
    and notification is always on first place of list. And last if vibration is enabled
    notification vibrates.
    
    Patchsets:
     - codestyle corrections
    
    Patchst
     - add PowerUsageSummary intent
    
    cheers
    
    Change-Id: I84476626b0ff5424454a1132ea4ef77a696070c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3533,6 +3533,19 @@ public final class Settings {
          */
         public static final String SYSTEM_POWER_ENABLE_CRT_ON = "system_power_enable_crt_on";
 
+        /**
+         * Battery warning preferences
+         *
+         * 0 = show dialog + play sound (default)
+         * 1 = fire notification + play sound
+         * 2 = show dialog only
+         * 3 = fire notification only
+         * 4 = play sound only
+         * 5 = none
+         * @hide
+         */
+        public static final String POWER_UI_LOW_BATTERY_WARNING_POLICY = "power_ui_low_battery_warning_policy";
+
     }
 
     /**
