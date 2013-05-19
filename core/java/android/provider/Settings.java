b9eaf9e89ec767d149793243f89e100307a91e82
tonyp <tonyptonyp@gmail.com>
2013-05-19 10:30:09 +0200

    Lockscreen Camera widget option (thx @mnm9994u) (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3252,6 +3252,11 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
         /**
+         * @hide
+          */
+        public static final String KG_CAMERA_WIDGET = "kg_camera_widget";
+        
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
