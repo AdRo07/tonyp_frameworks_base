bb4cbed4a00eb0e82db262219922f824028d594e
tonyp <tonyptonyp@gmail.com>
2013-08-24 02:32:56 +0200

    Revert "Add the possiblity of using external sdcard as application storage for user 0"
    This reverts commit ea762de11582a993402429f4a7874eef7d183d1b.

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -1274,9 +1274,7 @@ class MountService extends IMountService.Stub
         }
 
         final UserEnvironment userEnv = new UserEnvironment(user.getIdentifier());
-        // Use default storage directory for mounting, we are only telling the apps
-        // to use external sdcard for storage if user 0 is using secondary storage
-        final File path = userEnv.getDefaultExternalStorageDirectory();
+        final File path = userEnv.getExternalStorageDirectory();
         final StorageVolume volume = StorageVolume.fromTemplate(mEmulatedTemplate, path, user);
         volume.setStorageId(0);
         addVolumeLocked(volume);
