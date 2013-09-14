a0ef6a4281260491c38d258b8291655651c93db7
0xD34D <clark@scheffsblend.com>
2013-09-14 13:12:09 -0700

    Add active display to ChameleonOS [1/2]
    This is the ChameleonOS implementation of the active
    display feature, similar to that found on the Moto X.
    
    When enabled the screen will turn on and display the
    incoming notification.  The user can then open the app
    for the notification, dismiss the notification or unlock
    their device normally.
    
    Additional notifications, if any exist, will be displayed
    below the ring notification and can be viewed by touching
    the notification icon.  The user can simply slide their
    finger across these other notification to quickly view
    them.  The last one touched will remain in the ring and
    the user can then perform any of the actions listed above.
    
    PS2-PS12:
    All the changes that brought us to this current implementation.
    
    Change-Id: I51c17cec33b65dc60111a10c5ca2c449596ebe63
    
    Conflicts:
    	core/res/res/values/dimens.xml
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -602,6 +602,10 @@ public class PhoneStatusBar extends BaseStatusBar {
             // no window manager? good luck with that
         }
 
+        if (mRecreating) {
+            addActiveDisplayView();
+        }
+
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
 
@@ -2478,7 +2482,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         setAreThereNotifications();
     }
 
-    private boolean areLightsOn() {
+    public boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2496,6 +2500,10 @@ public class PhoneStatusBar extends BaseStatusBar {
             mWindowManagerService.statusBarVisibilityChanged(mSystemUiVisibility);
         } catch (RemoteException ex) {
         }
+    }
+
+    public void setNavigationBarLightsOn(boolean on, boolean force) {
+        mNavigationBarView.setLowProfile(!on, true, force);
     }
 
     @Override
