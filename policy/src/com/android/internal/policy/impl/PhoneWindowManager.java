0f0804841489d9c3dc850d527c5121b502633b0b
tonyp <tonyptonyp@gmail.com>
2013-10-03 19:11:56 +0200

    Add dynamic Quickmemo button action (2/2)
    new dynamic (installation sensitive) implementation
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -183,6 +183,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int KEY_ACTION_TORCH = 9;
     private static final int KEY_ACTION_SCREENSHOT = 10;
     private static final int KEY_ACTION_LAUNCH_CAMERA = 11;
+    private static final int KEY_ACTION_QUICKMEMO = 99;
 
     // Masks for checking presence of hardware keys.
     // Must match values in core/res/res/values/config.xml
@@ -894,20 +895,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final Runnable mRingerChordLongPress = new Runnable() {
         public void run() {
             // Do the switch
-            final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-            final int ringerMode = am.getRingerMode();
-            final VolumePanel volumePanel = new VolumePanel(ThemeUtils.createUiContext(mContext),
-                                                              (AudioService) getAudioService());
-            if (ringerMode == AudioManager.RINGER_MODE_NORMAL) {
-                boolean vibrateSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.VIBRATE_WHEN_RINGING, 0, UserHandle.USER_CURRENT) != 0;
-                am.setRingerMode(vibrateSetting ? AudioManager.RINGER_MODE_VIBRATE :
-                                   AudioManager.RINGER_MODE_SILENT);
-            } else {
-                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            if (isAppInstalled("com.lge.QuickClip")) {
+                mContext.sendBroadcast(new Intent("com.lge.QuickClip.action.START_QUICKCLIP"));
             }
-            volumePanel.postVolumeChanged(AudioManager.STREAM_RING,AudioManager.FLAG_SHOW_UI
-                                          | AudioManager.FLAG_VIBRATE);
+            else {
+                final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
+                final int ringerMode = am.getRingerMode();
+                final VolumePanel volumePanel = new VolumePanel(ThemeUtils.createUiContext(mContext),
+                                                                  (AudioService) getAudioService());
+                 if (ringerMode == AudioManager.RINGER_MODE_NORMAL) {
+                    boolean vibrateSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.VIBRATE_WHEN_RINGING, 0, UserHandle.USER_CURRENT) != 0;
+                    am.setRingerMode(vibrateSetting ? AudioManager.RINGER_MODE_VIBRATE :
+                                       AudioManager.RINGER_MODE_SILENT);
+                } else {
+                    am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                }
+                volumePanel.postVolumeChanged(AudioManager.STREAM_RING,AudioManager.FLAG_SHOW_UI
+                                              | AudioManager.FLAG_VIBRATE);
+            }
         }
     };
 
@@ -1004,6 +1010,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case KEY_ACTION_SCREENSHOT:
                 takeScreenshot();
+                break;
+            case KEY_ACTION_QUICKMEMO:
+                if (isAppInstalled("com.lge.QuickClip")) {
+                    mContext.sendBroadcast(new Intent("com.lge.QuickClip.action.START_QUICKCLIP"));
+                } else {
+                    Runnable quickMemoToast = new Runnable() {
+                    public void run() {
+                        Toast.makeText(mContext, "Quickmemo isn't installed!", Toast.LENGTH_LONG).show();
+                        }
+                    };
+                    mHandler.post(quickMemoToast);
+                }
                 break;
             default:
                 break;
@@ -4169,6 +4187,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mHandler.postDelayed(mScreenshotTimeout, 10000);
             }
         }
+    }
+
+    private boolean isAppInstalled(String uri) {
+        PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return true;
     }
 
     /** {@inheritDoc} */
